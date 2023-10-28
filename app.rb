bffe5cd2e64a57df02d94d71726da98717119081
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-10-28 19:55:54 +0000

    api print currency hash

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "http"
+require "json"
+api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+raw_api_data = HTTP.get(api_url)
+parsed_api_data = JSON.parse(raw_api_data)
+
+# Access the list of currencies
+currencies = parsed_api_data["currencies"]
+pp currencies
+# Print the list of currencies
+#currencies.each do |currency_code, currency_description|
+#  puts "#{currency_code}: #{currency_description}"
+#end
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  """
+  """
+  erb(:homepage)
 end
