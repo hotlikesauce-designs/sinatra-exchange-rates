8c89a8cf7309af111988a8655ce5563e2872c9f3
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-10-28 20:38:15 +0000

    pages flowing

--- app.rb
+++ app.rb
@@ -17,7 +17,22 @@ get("/") do
   erb(:homepage)
 end
 
-get("/:first_page") do
+#the :colon makes it a dynamic page!
+get("/:currency_from") do
+  raw_api_data = HTTP.get(api_url)
+  parsed_api_data = JSON.parse(raw_api_data)
 
-  erb(:first_page)
+  @currencies = parsed_api_data["currencies"]
+  erb(:currency_from)
+end
+
+get("/:currency_from/:currency_to") do
+  raw_api_data = HTTP.get(api_url)
+  parsed_api_data = JSON.parse(raw_api_data)
+  @currencies = parsed_api_data["currencies"]
+  
+  @currency_from = params.fetch("currency_from")
+  @currency_to = params.fetch("currency_to")
+
+  erb(:currency_to)
 end
