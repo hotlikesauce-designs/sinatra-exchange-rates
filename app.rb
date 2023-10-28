3c89000171a090e80e130710013fc80d8795bba3
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-10-28 20:04:48 +0000

    list all currencies on homepage

--- app.rb
+++ app.rb
@@ -5,19 +5,15 @@ require "http"
 require "json"
 api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
 
-raw_api_data = HTTP.get(api_url)
-parsed_api_data = JSON.parse(raw_api_data)
-
-# Access the list of currencies
-currencies = parsed_api_data["currencies"]
-pp currencies
 # Print the list of currencies
 #currencies.each do |currency_code, currency_description|
 #  puts "#{currency_code}: #{currency_description}"
 #end
 
 get("/") do
-  """
-  """
+  @raw_api_data = HTTP.get(api_url)
+  @parsed_api_data = JSON.parse(@raw_api_data)
+  @currencies = @parsed_api_data["currencies"]
+
   erb(:homepage)
 end
